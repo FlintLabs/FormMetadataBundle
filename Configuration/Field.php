b0d0900153887c325d87e9d0871a4b95ebbb7701
Cameron Manderson <cameronmanderson@gmail.com>
2011-09-19 09:47:45 +1000

    Updated the namespace

--- Configuration/Field.php
+++ Configuration/Field.php
@@ -7,10 +7,10 @@
  * For full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace FlintLabs\Bundle\FormAnnotationsBundle\Mapping;
+namespace FlintLabs\Bundle\FormMetadataBundle\Configuration;
 
 /**
- *
+ * Contains the configuration elements for the field
  * @author camm (camm@flintinteractive.com.au)
  */
 class Field 
