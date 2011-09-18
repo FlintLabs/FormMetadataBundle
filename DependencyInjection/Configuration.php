db4fd1494a95864d2b2c4cbd5431429564492830
Cameron Manderson <cameronmanderson@gmail.com>
2011-09-19 09:55:59 +1000

    Updated missed key

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -26,7 +26,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('flint_cms');
+        $rootNode = $treeBuilder->root('form_metadata');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
