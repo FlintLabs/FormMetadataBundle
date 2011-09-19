409102204346248d3d28f1cef8fb450cd1169782
Cameron Manderson <cameronmanderson@gmail.com>
2011-09-19 11:12:30 +1000

    Added a placeholder for reading yaml configurations for the form

---
+++ YamlReader.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Copyright Cameron Manderson (c) 2011 All rights reserved.
+ * Date: 19/09/11
+ */
+namespace FlintLabs\Bundle\FormMetadataBundle;
+/**
+ *
+ * @author camm (camm@flintinteractive.com.au)
+ */
+class YamlReader implements EntityMetadataReader
+{
+    /**
+     * Read the entity and create an associated metadata
+     * @param $entity
+     * @return null|FormMetadata
+     */
+    public function getMetadata($entity)
+    {
+        // TODO: Implement getMetadata() method.
+    }
+
+}
